fb3bd1c79a4699978cd655076854af1a0b669947
Aarthikavelayutham <169143822+Aarthikavelayutham@users.noreply.github.com>
2026-03-17 12:39:54 +0530

    Fix static files and superuser

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ python manage.py shell -c "
 from django.contrib.auth.models import User
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+print('Superuser ready')
 "
